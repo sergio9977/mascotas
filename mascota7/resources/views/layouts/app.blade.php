ef57fcf262aa67caff3463364e12a8049062e858
wilson-pc <wilverhidalgo@outlook.com>
2017-01-11 22:43:42 -0400

    foto de perfil circle

--- mascota7/resources/views/layouts/app.blade.php
+++ mascota7/resources/views/layouts/app.blade.php
@@ -71,7 +71,9 @@
 
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                     <img src="imgUser/{{ Auth::user()->urlimage }}" class="img-circle" alt="" style='max-width: 100px'>
+                                    {{ Auth::user()->name }}
+                                      <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
